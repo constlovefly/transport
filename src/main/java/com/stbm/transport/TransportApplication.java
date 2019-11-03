750b63419aa25cd7c22dea6f58749f99143269e2
constlovefly <6466761@qq.com>
2019-11-04 02:40:33 +0800

    IDEA整合GitHub,项目框架SpringBoot+Mybatis。

--- src/main/java/com/stbm/transport/TransportApplication.java
+++ src/main/java/com/stbm/transport/TransportApplication.java
@@ -2,12 +2,14 @@ package com.stbm.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 
 /**
  * @author Administrator
  */
 @SpringBootApplication
+@MapperScan("com.stbm.transport.daos")
 public class TransportApplication {
 
     public static void main(String[] args) {
