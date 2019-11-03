ae4e639376a226c347bfcad89af14044e05f8016
constlovefly <6466761@qq.com>
2019-11-04 02:46:08 +0800

    IDEA整合GitHub,项目框架SpringBoot+Mybatis。

--- src/main/java/com/stbm/transport/base/MyMapper.java
+++ src/main/java/com/stbm/transport/base/MyMapper.java
@@ -11,6 +11,6 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @Version: 1.0
  **/
 
-public interface MyMapper extends Mapper, MySqlMapper {
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
     //FIXME 特别注意，该接口不能被扫描到，否则会出错
 }
