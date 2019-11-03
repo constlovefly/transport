48348e586095080054301aa3694c02ea3cd7a9e3
constlovefly <6466761@qq.com>
2019-11-03 23:47:21 +0800

    IDEA整合GitHub,项目框架SpringBoot+Mybatis。

---
+++ src/main/java/com/stbm/transport/TransportApplication.java
@@ -0,0 +1,19 @@
+package com.stbm.transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class TransportApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TransportApplication.class, args);
+
+    }
+
+}
